62da50efa92de4d08f7ee5a66ea62e709e979f5f
jochen <jochen.reuter@andrena.de>
2017-07-12 17:26:27 +0200

    refactored tests

--- RomanToArabicTest/ConverterTest.cs
+++ RomanToArabicTest/ConverterTest.cs
@@ -6,18 +6,24 @@ namespace RomanToArabicTest
     [TestFixture]
     public class RomanToArabicTest
     {
+        private Converter _converter;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _converter = new Converter();
+        }
+
         [Test]
         public void Converter_ConvertI_Returns1()
         {
-            var converter = new Converter();
-            Assert.That(converter.Convert("I"), Is.EqualTo(1));
+            Assert.That(_converter.Convert("I"), Is.EqualTo(1));
         }
 
         [Test]
         public void Converter_ConvertII_Returns2()
         {
-            var converter = new Converter();
-            Assert.That(converter.Convert("II"), Is.EqualTo(2));
+            Assert.That(_converter.Convert("II"), Is.EqualTo(2));
         }
     }
 }
